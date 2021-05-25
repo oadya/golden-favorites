4c37ba376672839f7ca4c89d96e2da5424aa9d15
Adya Orphee <orphe.adya@gmail.com>
2021-05-25 22:32:21 +0200

    modification des icons du menu

--- src/app/domain/menu.ts
+++ src/app/domain/menu.ts
@@ -8,62 +8,62 @@ export let menu: NavItem[] = [
     },
     {
         displayName: 'Angular',
-        iconName: 'face',
+        iconName: '',
         route: 'angular',
         children: [
             {
                 displayName: 'Reactive form',
-                iconName: 'account_box',
+                iconName: '',
                 route: 'angular/rib-sepa'
             }
         ]
     },
     {
         displayName: 'Pages web',
-        iconName: 'face',
+        iconName: '',
         route: 'pages-web',
         children: [
             {
                 displayName: 'Business Website',
-                iconName: 'account_box',
+                iconName: '',
                 route: 'pages-web/business-website'
             },
             {
                 displayName: 'Ecommerce website',
-                iconName: 'account_box',
+                iconName: '',
                 route: 'pages-web/ecommerce-website'
             },
 
             {
                 displayName: 'Drop down menu',
-                iconName: 'account_box',
+                iconName: '',
                 route: 'pages-web/drop-down-menu'
             },
 
             {
                 displayName: 'Fitness website',
-                iconName: 'account_box',
+                iconName: '',
                 route: 'pages-web/fitness-website'
             },
 
             {
                 displayName: 'Login & Registration',
-                iconName: 'account_box',
+                iconName: '',
                 route: 'pages-web/login-registration'
             },
             {
                 displayName: 'Template website',
-                iconName: 'account_box',
+                iconName: '',
                 route: 'pages-web/template-website-one'
             },
             {
                 displayName: 'Flex Box',
-                iconName: 'account_box',
+                iconName: '',
                 route: 'pages-web/flex-box'
             },
             {
                 displayName: 'Sélecteurs CSS',
-                iconName: 'account_box',
+                iconName: '',
                 route: 'pages-web/selecteurs-css'
             }
         ]
